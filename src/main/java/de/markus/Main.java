fd489177ccc7f1e2b261f063b0d4121843c19d0e
markus <markus@ozzy>
2025-09-24 17:48:56 +0200

    This is the first commit from local!

---
+++ src/main/java/de/markus/Main.java
@@ -0,0 +1,30 @@
+package de.markus;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+	public static void main(String[] args) {
+        Frame frame = new Frame("Steph's Mini-App");
+        frame.setSize(400, 200);
+        frame.setLayout(new FlowLayout());
+
+        Label label = new Label("Hallo Steph!");
+        Button button = new Button("Klick mich");
+
+        button.addActionListener(e -> label.setText("Button gedrückt!"));
+
+        frame.add(label);
+        frame.add(button);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+
+        frame.setVisible(true);
+			//System.out.println("Hello Steph!");
+		}
+
+}
