003702eedbaa8f987a9e649a71753df1c677e3b9
Markus <markus.meier68@gmx.de>
2025-09-24 20:23:33 +0200

    Changed Titel of app

--- src/main/java/de/markus/Main.java
+++ src/main/java/de/markus/Main.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 
 public class Main {
 	public static void main(String[] args) {
-        Frame frame = new Frame("Steph's Inventory-App");
+        Frame frame = new Frame("Steph's Inventory-App AWT");
         frame.setSize(400, 200);
         frame.setLayout(new FlowLayout());
 
